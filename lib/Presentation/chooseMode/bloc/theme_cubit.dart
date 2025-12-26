9ddcb2c3c550986b6544c3e6e0732c1dc57e1e9a
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2025-12-26 10:58:55 +0200

    update list rendering

--- lib/Presentation/chooseMode/bloc/theme_cubit.dart
+++ lib/Presentation/chooseMode/bloc/theme_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:flutter/material.dart';
 
 class ThemeCubit extends HydratedCubit<ThemeMode> {
   ThemeCubit() : super(ThemeMode.system);
